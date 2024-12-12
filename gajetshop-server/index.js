cdc77484bbd175757972b5612479653ae44d891b
anikmk <anikmollik122321@gmail.com>
2024-12-12 11:03:47 +0600

    connect md successfully

--- gajetshop-server/index.js
+++ gajetshop-server/index.js
@@ -1,12 +1,43 @@
 const express = require('express');
+require('dotenv').config();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('GAJETSHOP SERVER IS READY')
 })
+
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@anik.34iapyi.mongodb.net/?retryWrites=true&w=majority&appName=Anik`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  catch(error) {
+    console.log(error)
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
